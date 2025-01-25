181cc9c1d5fe3d409276d8c5f2a60fe8a4405fe1
heyitssakinah <heyitssakinah@gmail.com>
2025-01-25 23:24:24 +0800

    trivial echo inputs

--- src/main/java/AbuHurairah.java
+++ src/main/java/AbuHurairah.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class AbuHurairah {
+    static String NAME = "Abu Hurairah";
+    static String LINE = "    ____________________________________________________________";
+
+    public static void serve(String s) { System.out.println(s); }
+
     public static void main(String[] args) {
-        String NAME = "Abu Hurairah";
-        String LINE = "____________________________________________________________";
-
-        System.out.println(LINE + "\n" + "Hello! I'm " + NAME + "\n" + "What can I do for you?\n" + LINE);
-        System.out.println("Bye. Hope to see you again soon!\n" + LINE );
-
+        // Welcome prompt
+        serve(LINE + "\n" + "     Assalamualaikum! I'm " + NAME
+                + "\n" + "     What can I do for you?\n" + LINE + "\n");
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            String request = input.nextLine();
+            if (request.equals("bye")) {
+                serve(LINE + "\n     Hope to see you again soon!\n" + LINE);
+                break;
+            }
+            serve(LINE + "\n     " + request + "\n" + LINE + "\n");
+        }
     }
 }
