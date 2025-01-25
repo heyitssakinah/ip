529c2414ccc09b922fa155d102ede1368513f58c
heyitssakinah <heyitssakinah@gmail.com>
2025-01-25 19:04:33 +0800

    Named my bot

---
+++ src/main/java/AbuHurairah.java
@@ -0,0 +1,10 @@
+public class AbuHurairah {
+    public static void main(String[] args) {
+        String NAME = "Abu Hurairah";
+        String LINE = "____________________________________________________________";
+
+        System.out.println(LINE + "\n" + "Hello! I'm " + NAME + "\n" + "What can I do for you?\n" + LINE);
+        System.out.println("Bye. Hope to see you again soon!\n" + LINE );
+
+    }
+}
