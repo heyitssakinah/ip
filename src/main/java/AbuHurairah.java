8dacae7ddf3c1f467642c476222252e629e30ece
heyitssakinah <heyitssakinah@gmail.com>
2025-01-26 00:20:27 +0800

    Add, List

--- src/main/java/AbuHurairah.java
+++ src/main/java/AbuHurairah.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class AbuHurairah {
     static String NAME = "Abu Hurairah";
@@ -10,14 +11,25 @@ public class AbuHurairah {
         // Welcome prompt
         serve(LINE + "\n" + "     Assalamualaikum! I'm " + NAME
                 + "\n" + "     What can I do for you?\n" + LINE + "\n");
+
+        ArrayList<String> list = new ArrayList<>();
+        // Get request
         Scanner input = new Scanner(System.in);
         while (true) {
             String request = input.nextLine();
             if (request.equals("bye")) {
                 serve(LINE + "\n     Hope to see you again soon!\n" + LINE);
                 break;
+            } else if (request.equals("list")) {
+                serve(LINE);
+                for (int i = 0; i < list.size(); i++) {
+                    serve("     " + (i + 1) + ". " + list.get(i));
+                }
+                serve(LINE + "\n");
+            } else {
+                list.add(request);
+                serve(LINE + "\n     " + "added: " + request + "\n" + LINE + "\n");
             }
-            serve(LINE + "\n     " + request + "\n" + LINE + "\n");
         }
     }
 }
